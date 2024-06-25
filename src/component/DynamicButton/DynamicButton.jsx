4e35db5e6c87421eeeb1fd082ef1fac8bc7ddf1f
achris-alonzo30 <achris.alonzo30@gmail.com>
2024-06-25 13:39:48 -0300

    create variant for the dynamic button

---
+++ src/component/DynamicButton/DynamicButton.jsx
@@ -0,0 +1,15 @@
+
+const variant = {
+    primary: "",
+    secondary: "",
+    destructive: ""
+}
+
+export const DynamicButton = () => {
+  return (
+    <button className="card__header--searchButton">
+      <span>+</span>
+      Add New Warehouse
+    </button>
+  );
+};
