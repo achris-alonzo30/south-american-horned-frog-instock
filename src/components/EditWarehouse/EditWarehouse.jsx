54bd4082a6e584662e473680bfc9c9294b2389a3
samreenaziz <samreen.aaziz@gmail.com>
2024-06-25 17:40:41 -0400

    progress on edit warehouse

--- src/components/EditWarehouse/EditWarehouse.jsx
+++ src/components/EditWarehouse/EditWarehouse.jsx
@@ -57,25 +57,29 @@ const EditWarehouse = ({warehouse}) => {
         <h1 className = "edit-wh__header"> Edit Warehouse </h1>
         <form className = "edit-wh__form" onSubmit = {submitHandler}>
 
-            <h2 className = "edit-wh__subheader"> Warehouse Details</h2>
-            <label className = "edit-wh-label" htmlFor="wh-name">Warehouse Name</label>
-            <input className = "edit-wh-input" type="text" id="wh-name" value={whName} onChange={(event) => setWhName(event.target.value)} placeholder = "Warehouse Name"/>
-            <label className = "edit-wh-label" htmlFor="wh-add">Street Address</label>
-            <input className = "edit-wh-input" type="text" id="wh-add" value={whAddress} onChange={(event) => setAddress(event.target.value)} placeholder = "Street Address"/>
-            <label className = "edit-wh-label" htmlFor="wh-city">City</label>
-            <input className = "edit-wh-input" type="text" id="wh-city" value={whCity} onChange={(event) => setCity(event.target.value)} placeholder = "City"/>
-            <label className = "edit-wh-label" htmlFor="wh-country">Warehouse Country</label>
-            <input className = "edit-wh-input" type="text" id="wh-country" value={whCountry}  onChange={(event) => setCountry(event.target.value)} placeholder = "Country"/>
+            <div className ="edit-wh__form-section warehouse-details">
+                <h2 className = "edit-wh__subheader"> Warehouse Details</h2>
+                <label className = "edit-wh-label" htmlFor="wh-name">Warehouse Name</label>
+                <input className = "edit-wh-input" type="text" id="wh-name" value={whName} onChange={(event) => setWhName(event.target.value)} placeholder = "Warehouse Name"/>
+                <label className = "edit-wh-label" htmlFor="wh-add">Street Address</label>
+                <input className = "edit-wh-input" type="text" id="wh-add" value={whAddress} onChange={(event) => setAddress(event.target.value)} placeholder = "Street Address"/>
+                <label className = "edit-wh-label" htmlFor="wh-city">City</label>
+                <input className = "edit-wh-input" type="text" id="wh-city" value={whCity} onChange={(event) => setCity(event.target.value)} placeholder = "City"/>
+                <label className = "edit-wh-label" htmlFor="wh-country">Warehouse Country</label>
+                <input className = "edit-wh-input" type="text" id="wh-country" value={whCountry}  onChange={(event) => setCountry(event.target.value)} placeholder = "Country"/>
+            </div>
 
-            <h2 className = "edit-wh__subheader"> Contact Details</h2>
-            <label className = "edit-wh-label" htmlFor="wh-contact-name">Contact Name</label>
-            <input className = "edit-wh-input" type="text" id="wh-name" value={contactName} onChange={(event) => setContactName(event.target.value)} placeholder = "Contact Name"/>
-            <label className = "edit-wh-label" htmlFor="wh-contact-pstn">Contact Position</label>
-            <input className = "edit-wh-input" type="text" id="wh-contact-pstn" value={contactPosition} onChange={(event) => setContactPosition(event.target.value)} placeholder = "Contact Position"/>
-            <label className = "edit-wh-label" htmlFor="wh-contact-phn">Phone Number</label>
-            <input className = "edit-wh-input" type="text" id="wh-contact-phn" value={contactPhn} onChange={(event) => setContactPhn(event.target.value)} placeholder = "Phone Number"/>
-            <label className = "edit-wh-label" htmlFor="wh-email">Email </label>
-            <input className = "edit-wh-input" type="text" id="wh-email" value={contactEmail} onChange={(event) => setContactEmail(event.target.value)} placeholder = "Email"/>
+            <div className ="edit-wh__form-section contact-details">
+                <h2 className = "edit-wh__subheader"> Contact Details</h2>
+                <label className = "edit-wh-label" htmlFor="wh-contact-name">Contact Name</label>
+                <input className = "edit-wh-input" type="text" id="wh-name" value={contactName} onChange={(event) => setContactName(event.target.value)} placeholder = "Contact Name"/>
+                <label className = "edit-wh-label" htmlFor="wh-contact-pstn">Contact Position</label>
+                <input className = "edit-wh-input" type="text" id="wh-contact-pstn" value={contactPosition} onChange={(event) => setContactPosition(event.target.value)} placeholder = "Contact Position"/>
+                <label className = "edit-wh-label" htmlFor="wh-contact-phn">Phone Number</label>
+                <input className = "edit-wh-input" type="text" id="wh-contact-phn" value={contactPhn} onChange={(event) => setContactPhn(event.target.value)} placeholder = "Phone Number"/>
+                <label className = "edit-wh-label" htmlFor="wh-email">Email </label>
+                <input className = "edit-wh-input" type="text" id="wh-email" value={contactEmail} onChange={(event) => setContactEmail(event.target.value)} placeholder = "Email"/>
+            </div>
 
             <div className = "cancel-submit__container">
                 <button type="cancel" className="edit-wh__cancel-button" onClick={cancelHandler}>Cancel</button>
