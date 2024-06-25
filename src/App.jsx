2fa3d4824fe835fcbcb9a31c2e6da797976a6fd9
jfitt149 <jfitt149@gmail.com>
2024-06-24 22:22:31 -0300

    finish footer

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,13 @@ import { useState } from "react";
 import "./App.scss";
 
 import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <Header></Header>
+      <Footer></Footer>
     </>
   );
 }
