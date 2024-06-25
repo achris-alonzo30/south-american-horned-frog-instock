65f971af7a18e78de44b087e35612116959b1125
achris-alonzo30 <achris.alonzo30@gmail.com>
2024-06-25 16:47:40 -0300

    add the option to make the input to radio button

--- src/component/DynamicInput/DynamicInput.jsx
+++ src/component/DynamicInput/DynamicInput.jsx
@@ -1,39 +1,45 @@
 import "./DynamicInput.scss";
 
-export const DynamicInput = ({ 
-    id, 
-    icon, 
-    type, 
-    select, 
-    radioName,
-    placeholder,
-    disabled = false,
+export const DynamicInput = ({
+  id,
+  icon,
+  type,
+  radioName,
+  placeholder,
+  disabled = false,
 }) => {
-  return (
-    <>
-      <div className="field">
+  if (radioName) {
+    return (
+      <label
+        htmlFor={id}
+        className={`field__radio ${disabled && "field__radio--inactive"}`}
+      >
         <input
           id={id}
           name={id}
-          type={type}
-          placeholder={placeholder}
-          className="field__input field__input--error"
-        />
-        {icon && <img src={icon} alt="Dynamic Icon" className="field__icon" />}
-      </div>
-      <label htmlFor={id} className={`field__radio ${disabled && "field__radio--inactive"}`}>
-        <input 
-            id={id} 
-            name={id} 
-            type="radio" 
-            value={radioName} 
-            className="field__radio--button" 
+          type="radio"
+          value={radioName}
+          className="field__radio--button"
         />
         {radioName}
       </label>
-      <select>
-        
-      </select>
-    </>
-  );
+    );
+  } else {
+    return (
+      <>
+        <div className="field">
+          <input
+            id={id}
+            name={id}
+            type={type}
+            placeholder={placeholder}
+            className="field__input field__input--error"
+          />
+          {icon && (
+            <img src={icon} alt="Dynamic Icon" className="field__icon" />
+          )}
+        </div>
+      </>
+    );
+  }
 };
--- src/component/MainBrowser/MainBrowser.jsx
+++ src/component/MainBrowser/MainBrowser.jsx
@@ -8,46 +8,39 @@ import arrowLeft from "../../assets/icons/arrow_back-24px.svg";
 import { DynamicButton } from "../DynamicButton/DynamicButton";
 import { DynamicInput } from "../DynamicInput/DynamicInput";
 
-
-export const MainBrowser = ({
-  
-  children,
-  browserName,
-  isSearch,
-}) => {
+export const MainBrowser = ({ children, browserName, isSearch }) => {
   const navigate = useNavigate();
 
   const goBack = () => {
     navigate(-1);
-  }
+  };
 
   return (
     <main className="card">
       <section className="card__header">
         <aside className="card__header--title">
-            <img 
-              src={arrowLeft} 
-              onClick={goBack}
-              className="card__header--arrow"
-              alt="Arrow Point Left in Indigo Color" 
-            />
-            <h1 className="card__header--pageName">{browserName}</h1>
+          <img
+            src={arrowLeft}
+            onClick={goBack}
+            className="card__header--arrow"
+            alt="Arrow Point Left in Indigo Color"
+          />
+          <h1 className="card__header--pageName">{browserName}</h1>
         </aside>
 
         {/* render this conditionally */}
         <aside className="card__header--search">
-          <DynamicInput 
-            type="text"
-            id="search"
-            icon={searchIcon} 
-            placeholder="Search..."
-          />
+            <DynamicInput
+              type="text"
+              id="search"
+              icon={searchIcon}
+              placeholder="Search..."
+            />
           <DynamicButton variant="add" />
-          
         </aside>
       </section>
 
-      { children }
+      {children}
     </main>
   );
 };
