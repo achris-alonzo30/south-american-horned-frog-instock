46fc2c146777bfc5c8eaefcf4b28a1b89dc054d4
Yvette de Sereville <ydesereville@uwaterloo.ca>
2024-06-26 16:40:42 -0400

    update inventory page .jsx with relevant props

--- src/pages/Inventory/Inventory.jsx
+++ src/pages/Inventory/Inventory.jsx
@@ -1,14 +1,27 @@
-import "./Inventory.scss";
+import "./Invetory.scss";
+
+import { useState, useEffect } from "react";
+import { getAllInventories } from "../../lib/api-inventories";
 
 import searchIcon from "../../assets/icons/search-24px.svg";
 
 import { Card } from "../../components/Card/Card";
 import { CardHeader } from "../../components/CardHeader/CardHeader";
-import { DynamicButton } from "../../components/DynamicButton/DynamicButton";
+import { CardFooter } from "../../components/CardFooter/CardFooter";
 import { DynamicInput } from "../../components/DynamicInput/DynamicInput";
+import { DynamicButton } from "../../components/DynamicButton/DynamicButton";
 import { InventoryList } from "../../components/InventoryList/InventoryList";
 
 export const Inventory = () => {
+  const [inventories, setInventories] = useState([]);
+
+  useEffect(() => {
+    getAllInventories(setInventories);
+  }, []);
+
+  // Add Loading here
+  if (!inventories) return <></>;
+
   return (
     <main className="main">
       <Card>
@@ -21,7 +34,8 @@ export const Inventory = () => {
           />
           <DynamicButton variant="add" addButtonName="Add New Item" />
         </CardHeader>
-        <InventoryList />
+        <InventoryList inventories={inventories} />
+        <CardFooter></CardFooter>
       </Card>
     </main>
   );
