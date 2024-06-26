c4b8a9a359d4f13e35462f2260f1d5ce08e10d29
achris-alonzo30 <achris.alonzo30@gmail.com>
2024-06-25 23:43:37 -0300

    start working on the arehouse detail

--- src/components/WarehouseDetails/WarehouseDetails.jsx
+++ src/components/WarehouseDetails/WarehouseDetails.jsx
@@ -12,20 +12,30 @@ export const WarehouseDetails = () => {
   return (
     <main className="main">
       <Card>
-        <CardHeader flexStyle="flexCol" browserName="Warehouse" className="">
-          <DynamicInput
-            type="text"
-            id="search"
-            icon={searchIcon}
-            placeholder="Search..."
-          />
-          <DynamicButton variant="add" addButtonName="Add New Warehouse" />
+        <CardHeader flexStyle="flexRow" browserName="Washington" withArrow>
+          <DynamicButton variant="edit" />
         </CardHeader>
+        <section>
+          <hgroup>
+            <h4>WAREHOUSE ADDRES:</h4>
+            <p>33 Pearl Street SW, 
+              Washington, USA</p>
+          </hgroup>
+
+          <aside>
+            <hgroup>
+              <h4>CONTACT NAME:</h4>
+              <p>Graeme Lyon </p>
+              <p>Warehouse Manager</p>
+            </hgroup>
+            <hgroup>
+              <h4>CONTACT INFORMATION:</h4>
+              <p>+1 (647) 504-0911</p>
+              <a href="mailto:glyon@instock.com">glyon@instock.com</a>
+            </hgroup>
+          </aside>
+        </section>
         <WarehouseList />
-        <CardFooter>
-          <DynamicButton variant="cancel" />
-          <DynamicButton variant="save" />
-        </CardFooter>
       </Card>
     </main>
   );
