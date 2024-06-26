5e162f4478e45de9a749dae594fe4430b801125b
Yvette de Sereville <ydesereville@uwaterloo.ca>
2024-06-26 11:56:06 -0400

    add Inventory List skeleton -- no api data functionality

---
+++ src/components/InventoryList/InventoryList.jsx
@@ -0,0 +1,310 @@
+import "./InventoryList.scss";
+
+import { Link } from "react-router-dom";
+
+import sortIcon from "../../assets/icons/sort-24px.svg";
+import editIcon from "../../assets/icons/edit_indigo-24px.svg";
+import trashIcon from "../../assets/icons/delete_outline-24px.svg";
+import chevronRight from "../../assets/icons/chevron_right-24px.svg";
+
+export const InventoryList = () => {
+  return (
+    <table className="table">
+      <thead className="table__header">
+        <tr className="table__header--row">
+          <th className="table__header--cells">
+            <div className="table__header--content">
+              INVENTORY
+              <img
+                src={sortIcon}
+                className="table__header--icon"
+                alt="Two Arrows Point Up and Down"
+              />
+            </div>
+          </th>
+          <th className="table__header--cells">
+            <div className="table__header--content">
+              CATEGORY
+              <img
+                src={sortIcon}
+                className="table__header--icon"
+                alt="Two Arrows Point Up and Down"
+              />
+            </div>
+          </th>
+          <th className="table__header--cells">
+            <div className="table__header--content">
+              STATUS
+              <img
+                src={sortIcon}
+                className="table__header--icon"
+                alt="Two Arrows Point Up and Down"
+              />
+            </div>
+          </th>
+          <th className="table__header--cells">
+            <div className="table__header--content">
+              QTY
+              <img
+                src={sortIcon}
+                className="table__header--icon"
+                alt="Two Arrows Point Up and Down"
+              />
+            </div>
+          </th>
+          <th className="table__header--cells">
+            <div className="table__header--content">
+              WAREHOUSE
+              <img
+                src={sortIcon}
+                className="table__header--icon"
+                alt="Two Arrows Point Up and Down"
+              />
+            </div>
+          </th>
+          <th className="table__header--end">
+            <div className="table__header--content ">ACTIONS</div>
+          </th>
+        </tr>
+      </thead>
+
+      <tbody className="table__body">
+        <tr className="table__body--row">
+          <td className="table__data--cells table__data--location">
+            <h4 className="table__data--header table__data--hidden ">
+              WAREHOUSE
+            </h4>
+            <Link to="/" className="table__data--link">
+              Manhattan
+              <img src={chevronRight} alt="Arrow Point To Right" />
+            </Link>
+          </td>
+          <td className="table__data--cells table__data--name">
+            <h2 className="table__data--header table__data--hidden">
+              CONTACT NAME
+            </h2>
+            <p className="table__data--content">Parmin Aujla</p>
+          </td>
+          <td className="table__data--cells table__data--address">
+            <h2 className="table__data--header table__data--hidden">ADDRESS</h2>
+            <p className="table__data--content table__data--breakpoint">
+              503 Broadway New York, USA
+            </p>
+          </td>
+          <td className="table__data--cells table__data--contact">
+            <h2 className="table__data--header table__data--hidden">
+              CONTACT INFORMATION
+            </h2>
+            <p className="table__data--content">+1 (629) 555-0129</p>
+            <a
+              href="mailto:paujla@instock.com"
+              className="table__data--content"
+            >
+              paujla@instock.com
+            </a>
+          </td>
+          <td className="table__data--actions">
+            <img
+              src={trashIcon}
+              alt="Garbage Red Color Icon"
+              className="table__content--delete"
+            />
+            <img
+              src={editIcon}
+              alt="Garbage Red Color Icon"
+              className="table__content--edit"
+            />
+          </td>
+        </tr>
+        <tr className="table__body--row">
+          <td className="table__data--cells table__data--location">
+            <h4 className="table__data--header table__data--hidden ">
+              WAREHOUSE
+            </h4>
+            <Link to="/" className="table__data--link">
+              Manhattan
+              <img src={chevronRight} alt="Arrow Point To Right" />
+            </Link>
+          </td>
+          <td className="table__data--cells table__data--name">
+            <h2 className="table__data--header table__data--hidden">
+              CONTACT NAME
+            </h2>
+            <p className="table__data--content">Parmin Aujla</p>
+          </td>
+          <td className="table__data--cells table__data--address">
+            <h2 className="table__data--header table__data--hidden">ADDRESS</h2>
+            <p className="table__data--content table__data--breakpoint">
+              503 Broadway New York, USA
+            </p>
+          </td>
+          <td className="table__data--cells table__data--contact">
+            <h2 className="table__data--header table__data--hidden">
+              CONTACT INFORMATION
+            </h2>
+            <p className="table__data--content">+1 (629) 555-0129</p>
+            <a
+              href="mailto:paujla@instock.com"
+              className="table__data--content"
+            >
+              paujla@instock.com
+            </a>
+          </td>
+          <td className="table__data--actions">
+            <img
+              src={trashIcon}
+              alt="Garbage Red Color Icon"
+              className="table__content--delete"
+            />
+            <img
+              src={editIcon}
+              alt="Garbage Red Color Icon"
+              className="table__content--edit"
+            />
+          </td>
+        </tr>
+        <tr className="table__body--row">
+          <td className="table__data--cells table__data--location">
+            <h4 className="table__data--header table__data--hidden ">
+              WAREHOUSE
+            </h4>
+            <Link to="/" className="table__data--link">
+              Manhattan
+              <img src={chevronRight} alt="Arrow Point To Right" />
+            </Link>
+          </td>
+          <td className="table__data--cells table__data--name">
+            <h2 className="table__data--header table__data--hidden">
+              CONTACT NAME
+            </h2>
+            <p className="table__data--content">Parmin Aujla</p>
+          </td>
+          <td className="table__data--cells table__data--address">
+            <h2 className="table__data--header table__data--hidden">ADDRESS</h2>
+            <p className="table__data--content table__data--breakpoint">
+              503 Broadway New York, USA
+            </p>
+          </td>
+          <td className="table__data--cells table__data--contact">
+            <h2 className="table__data--header table__data--hidden">
+              CONTACT INFORMATION
+            </h2>
+            <p className="table__data--content">+1 (629) 555-0129</p>
+            <a
+              href="mailto:paujla@instock.com"
+              className="table__data--content"
+            >
+              paujla@instock.com
+            </a>
+          </td>
+          <td className="table__data--actions">
+            <img
+              src={trashIcon}
+              alt="Garbage Red Color Icon"
+              className="table__content--delete"
+            />
+            <img
+              src={editIcon}
+              alt="Garbage Red Color Icon"
+              className="table__content--edit"
+            />
+          </td>
+        </tr>
+        <tr className="table__body--row">
+          <td className="table__data--cells table__data--location">
+            <h4 className="table__data--header table__data--hidden ">
+              WAREHOUSE
+            </h4>
+            <Link to="/" className="table__data--link">
+              Manhattan
+              <img src={chevronRight} alt="Arrow Point To Right" />
+            </Link>
+          </td>
+          <td className="table__data--cells table__data--name">
+            <h2 className="table__data--header table__data--hidden">
+              CONTACT NAME
+            </h2>
+            <p className="table__data--content">Parmin Aujla</p>
+          </td>
+          <td className="table__data--cells table__data--address">
+            <h2 className="table__data--header table__data--hidden">ADDRESS</h2>
+            <p className="table__data--content table__data--breakpoint">
+              503 Broadway New York, USA
+            </p>
+          </td>
+          <td className="table__data--cells table__data--contact">
+            <h2 className="table__data--header table__data--hidden">
+              CONTACT INFORMATION
+            </h2>
+            <p className="table__data--content">+1 (629) 555-0129</p>
+            <a
+              href="mailto:paujla@instock.com"
+              className="table__data--content"
+            >
+              paujla@instock.com
+            </a>
+          </td>
+          <td className="table__data--actions">
+            <img
+              src={trashIcon}
+              alt="Garbage Red Color Icon"
+              className="table__content--delete"
+            />
+            <img
+              src={editIcon}
+              alt="Garbage Red Color Icon"
+              className="table__content--edit"
+            />
+          </td>
+        </tr>
+        <tr className="table__body--row">
+          <td className="table__data--cells table__data--location">
+            <h4 className="table__data--header table__data--hidden ">
+              WAREHOUSE
+            </h4>
+            <Link to="/" className="table__data--link">
+              <p className="table__data--text">Manhattan</p>
+              <img src={chevronRight} alt="Arrow Point To Right" />
+            </Link>
+          </td>
+          <td className="table__data--cells table__data--name">
+            <h2 className="table__data--header table__data--hidden">
+              CONTACT NAME
+            </h2>
+            <p className="table__data--content">Parmin Aujla</p>
+          </td>
+          <td className="table__data--cells table__data--address">
+            <h2 className="table__data--header table__data--hidden">ADDRESS</h2>
+            <p className="table__data--content table__data--breakpoint">
+              503 Broadway New York, USA
+            </p>
+          </td>
+          <td className="table__data--cells table__data--contact">
+            <h2 className="table__data--header table__data--hidden">
+              CONTACT INFORMATION
+            </h2>
+            <p className="table__data--content">+1 (629) 555-0129</p>
+            <a
+              href="mailto:paujla@instock.com"
+              className="table__data--content"
+            >
+              paujla@instock.com
+            </a>
+          </td>
+          <td className="table__data--actions">
+            <img
+              src={trashIcon}
+              alt="Garbage Red Color Icon"
+              className="table__data--delete"
+            />
+            <img
+              src={editIcon}
+              alt="Garbage Red Color Icon"
+              className="table__data--edit"
+            />
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  );
+};
