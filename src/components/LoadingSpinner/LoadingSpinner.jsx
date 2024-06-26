38c3424e168ed193d87c77d4e77d216f56755893
achris-alonzo30 <achris.alonzo30@gmail.com>
2024-06-26 16:12:45 -0300

    create the jsx data for the loading spinner component

--- src/components/LoadingSpinner/LoadingSpinner.jsx
+++ src/components/LoadingSpinner/LoadingSpinner.jsx
@@ -1,7 +1,16 @@
-import "./LoadingSpinner.scss"
+import "./LoadingSpinner.scss";
+
+import loaderLogo from "../../assets/icons/loader.svg";
 
 export const LoadingSpinner = () => {
-    return (
-        <></>
-    )
-}
+  return (
+    <main className="loader">
+      <img
+        src={loadingLogo}
+        alt="Loading Spinner Logo"
+        className="loader__logo"
+      />
+      <p className="loader__text">Loading...</p>
+    </main>
+  );
+};
