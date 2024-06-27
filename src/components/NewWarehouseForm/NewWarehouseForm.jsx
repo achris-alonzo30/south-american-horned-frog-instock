fd89262db044075823973a5b712d19f6f45ca7ba
jfitt149 <jfitt149@gmail.com>
2024-06-27 15:19:33 -0300

    working on form

--- src/components/NewWarehouseForm/NewWarehouseForm.jsx
+++ src/components/NewWarehouseForm/NewWarehouseForm.jsx
@@ -1,20 +1,14 @@
 import "./NewWarehouseForm.scss";
 
-import { postWarehouse } from "../../lib/api-warehouses";
 import { useState, useEffect } from "react";
 
-function NewWarehouseForm() {
-  const [formValues, setFormValues] = useState({
-    warehouse_name: "",
-    address: "",
-    city: "",
-    country: "",
-    contact_name: "",
-    contact_position: "",
-    contact_phone: "",
-    contact_email: "",
-  });
-
+function NewWarehouseForm({
+  formValues,
+  setFormValues,
+  handleSubmit,
+  isError,
+  setError,
+}) {
   const handleInputChange = (e) => {
     const { name, value } = e.target;
     setFormValues({
@@ -22,97 +16,98 @@ function NewWarehouseForm() {
       [name]: value,
     });
   };
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    const { name, value } = e.target;
-    setFormValues({
-      ...formValues,
-      [name]: value,
-    });
 
-    postWarehouse(formValues);
-  };
+  // const handleSubmit = async (e) => {
+  //   e.preventDefault();
+  //   const { name, value } = e.target;
+  //   setFormValues({
+  //     ...formValues,
+  //     [name]: value,
+  //   });
+
+  //   postWarehouse(formValues);
+  // };
 
   return (
     <form className="form" onSubmit={handleSubmit}>
-      <div>
-        <h2>Warehouse Details</h2>
-        <label htmlFor="">
-          Warehouse Name
-          <input
-            type="text"
-            name="warehouse_name"
-            value={formValues.warehouse_name}
-            placeholder="Warehouse Name"
-          />
-        </label>
+      <div className="form__content form__content--warehouse-details">
+        <h2 className="form__title">Warehouse Details</h2>
+        <label className="form__label">Warehouse Name</label>
+        <input
+          type="text"
+          className={`form__input ${isError ? "form__input--error" : ""}`}
+          name="warehouse_name"
+          value={formValues.warehouse_name}
+          onChange={handleInputChange}
+          placeholder="Warehouse Name"
+        />
 
-        <label htmlFor="">
-          Street Adress
-          <input
-            type="text"
-            name="address"
-            value={formValues.address}
-            placeholder="Street Adress"
-          />
-        </label>
-        <label htmlFor="">
-          City
-          <input
-            type="text"
-            name="city"
-            value={formValues.city}
-            placeholder="City"
-          />
-        </label>
-        <label htmlFor="">
-          Country
-          <input
-            type="text"
-            name="country"
-            value={formValues.country}
-            placeholder="Country"
-          />
-        </label>
+        <label className="form__label">Street Adress</label>
+        <input
+          className={`form__input ${isError ? "form__input--error" : ""}`}
+          type="text"
+          name="address"
+          value={formValues.address}
+          onChange={handleInputChange}
+          placeholder="Street Adress"
+        />
+        <label className="form__label">City</label>
+        <input
+          className={`form__input ${isError ? "form__input--error" : ""}`}
+          type="text"
+          name="city"
+          value={formValues.city}
+          onChange={handleInputChange}
+          placeholder="City"
+        />
+        <label className="form__label">Country</label>
+        <input
+          className={`form__input ${isError ? "form__input--error" : ""}`}
+          type="text"
+          name="country"
+          value={formValues.country}
+          onChange={handleInputChange}
+          placeholder="Country"
+        />
       </div>
-      <div>
-        <h2>Contact Details</h2>
-        <label htmlFor="">
-          Contact Name
-          <input
-            type="text"
-            name="contact_name"
-            value={formValues.contact_name}
-            placeholder="Contact Name"
-          />
-        </label>
-        <label htmlFor="">
-          Position
-          <input
-            type="text"
-            name="contact_position"
-            value={formValues.contact_position}
-            placeholder="Position"
-          />
-        </label>
-        <label htmlFor="">
-          Phone Number
-          <input
-            type="text"
-            name="contact_phone"
-            value={formValues.contact_phone}
-            placeholder="Phone Number"
-          />
-        </label>
-        <label htmlFor="">
-          Email
-          <input
-            type="text"
-            name="contact_email"
-            value={formValues.contact_email}
-            placeholder="Email"
-          />
-        </label>
+      <div className="form__content form__content--contact-details">
+        <h2 className="form__title">Contact Details</h2>
+        <label className="form__label">Contact Name</label>
+        <input
+          className={`form__input ${isError ? "form__input--error" : ""}`}
+          type="text"
+          name="contact_name"
+          value={formValues.contact_name}
+          onChange={handleInputChange}
+          placeholder="Contact Name"
+        />
+        <label className="form__label">Position</label>
+        <input
+          className={`form__input ${isError ? "form__input--error" : ""}`}
+          type="text"
+          name="contact_position"
+          value={formValues.contact_position}
+          onChange={handleInputChange}
+          placeholder="Position"
+        />
+        <label className="form__label">Phone Number</label>
+        <input
+          className={`form__input ${isError ? "form__input--error" : ""}`}
+          type="text"
+          name="contact_phone"
+          value={formValues.contact_phone}
+          onChange={handleInputChange}
+          placeholder="Phone Number"
+        />
+        <label className="form__label">Email</label>
+        <input
+          className={`form__input ${isError ? "form__input--error" : ""}`}
+          type="text"
+          name="contact_email"
+          value={formValues.contact_email}
+          onChange={handleInputChange}
+          placeholder="Email"
+        />
       </div>
     </form>
   );
--- src/pages/NewWarehouse/NewWarehouse.jsx
+++ src/pages/NewWarehouse/NewWarehouse.jsx
@@ -1,8 +1,8 @@
 import "./NewWarehouse.scss";
 
 import { useState, useEffect } from "react";
-import { Link, useParams } from "react-router-dom";
-// import { postWarehouse } from "../../lib/api-warehouses";
+import { Link, useParams, useNavigate } from "react-router-dom";
+import { postWarehouse } from "../../lib/api-warehouses";
 
 import { Card } from "../../components/Card/Card";
 import { CardHeader } from "../../components/CardHeader/CardHeader";
@@ -12,6 +12,43 @@ import NewWarehouseForm from "../../components/NewWarehouseForm/NewWarehouseForm
 // import { LoadingSpinner } from "../../components/LoadingSpinner/LoadingSpinner";
 
 function NewWarehouse() {
+  const [formValues, setFormValues] = useState({
+    warehouse_name: "",
+    address: "",
+    city: "",
+    country: "",
+    contact_name: "",
+    contact_position: "",
+    contact_phone: "",
+    contact_email: "",
+  });
+  const [isError, setError] = useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const { name, value } = e.target;
+    setFormValues({
+      ...formValues,
+      [name]: value,
+    });
+    if (!value) {
+      setError(true);
+      return;
+    } else {
+      setError(false);
+    }
+    postWarehouse(formValues);
+  };
+
+  const navigate = useNavigate();
+
+  const handleCancel = (e) => {
+    e.preventDefault();
+    navigate("/");
+  };
+
+  console.log(formValues);
+
   return (
     <main className="main">
       <Card>
@@ -20,12 +57,23 @@ function NewWarehouse() {
           browserName="Add New Warehouse"
           withArrow
         ></CardHeader>
-        <NewWarehouseForm></NewWarehouseForm>
+        <NewWarehouseForm
+          formValues={formValues}
+          setFormValues={setFormValues}
+          handleSubmit={handleSubmit}
+          isError={isError}
+          setError={setError}
+        ></NewWarehouseForm>
         <CardFooter>
-          <DynamicButton variant="cancel"></DynamicButton>
+          <DynamicButton
+            variant="cancel"
+            onClick={handleCancel}
+          ></DynamicButton>
           <DynamicButton
             variant="add"
             addButtonName="Add Warehouse"
+            type="submit"
+            onClick={handleSubmit}
           ></DynamicButton>
         </CardFooter>
       </Card>
