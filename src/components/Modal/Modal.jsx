418d5a3bbb10b1fe384e41a4effa738e6a5de9ed
achris-alonzo30 <achris.alonzo30@gmail.com>
2024-06-27 13:21:20 -0300

    make card modal component responsive across different screen size

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -9,27 +9,23 @@ export const Modal = ({ onClose, onDelete }) => {
   return (
     <main className="modal">
       <section className="modal__card">
-
-        <div className="modal__card--body">
-          <nav className="modal__header">
-            <button onClick={onClose} className="modal__close--button">
-              <img src={closeIcon} alt="X icon" />
-            </button>
-          </nav>
-          <hgroup className="modal__body">
-            <h2 className="modal__body--heading">Delete Washington warehouse?</h2>
-            <p className="modal__body--paragraph">
-              Please confirm that you d like to delete the Washington from the
-              list of warehouses. You won t be able to undo this action.
-            </p>
-          </hgroup>
-        </div>
+        <nav className="modal__header">
+          <button onClick={onClose} className="modal__close--button">
+            <img src={closeIcon} alt="X icon" />
+          </button>
+        </nav>
+        <hgroup className="modal__body">
+          <h2 className="modal__body--heading">Delete Washington warehouse?</h2>
+          <p className="modal__body--paragraph">
+            Please confirm that you d like to delete the Washington from the
+            list of warehouses. You won t be able to undo this action.
+          </p>
+        </hgroup>
 
         <nav className="modal__footer">
           <DynamicButton variant="cancel" onClick={onClose} type="button" />
           <DynamicButton variant="delete" onClick={onDelete} type="button" />
         </nav>
-        
       </section>
     </main>
   );
