1850c3af1e475eb31182083c73c05b9b0e4a7f5b
achris-alonzo30 <achris.alonzo30@gmail.com>
2024-06-27 13:35:37 -0300

    move the modal state and where it is imported to WarehouseList

--- src/components/WarehouseList/WarehouseList.jsx
+++ src/components/WarehouseList/WarehouseList.jsx
@@ -1,127 +1,155 @@
 import "./WarehouseList.scss";
 
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import { deleteWarehouse } from "../../lib/api-warehouses";
+
+import { Modal } from "../Modal/Modal";
 
 import sortIcon from "../../assets/icons/sort-24px.svg";
 import editIcon from "../../assets/icons/edit_indigo-24px.svg";
 import trashIcon from "../../assets/icons/delete_outline-24px.svg";
 import chevronRight from "../../assets/icons/chevron_right-24px.svg";
 
-export const WarehouseList = ({ warehouses }) => {
+export const WarehouseList = ({ warehouses, onModalOpen }) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleOpenModal = () => setIsModalOpen(!isModalOpen);
+
+  const handleDeleteWarehouse = async (id) => {
+    await deleteWarehouse(id);
+  }
+
   return (
-    <table className="table">
-      <thead className="table__header">
-        <tr className="table__header--row">
-          <th className="table__header--cells">
-            <div className="table__header--content">
-              WAREHOUSE
-              <img
-                src={sortIcon}
-                className="table__header--icon"
-                alt="Two Arrows Point Up and Down"
-              />
-            </div>
-          </th>
-          <th className="table__header--cells">
-            <div className="table__header--content">
-              ADDRESS
-              <img
-                src={sortIcon}
-                className="table__header--icon"
-                alt="Two Arrows Point Up and Down"
-              />
-            </div>
-          </th>
-          <th className="table__header--cells">
-            <div className="table__header--content">
-              CONTACT NAME
-              <img
-                src={sortIcon}
-                className="table__header--icon"
-                alt="Two Arrows Point Up and Down"
-              />
-            </div>
-          </th>
-          <th className="table__header--cells">
-            <div className="table__header--content">
-              CONTACT INFORMATION
-              <img
-                src={sortIcon}
-                className="table__header--icon"
-                alt="Two Arrows Point Up and Down"
-              />
-            </div>
-          </th>
-          <th className="table__header--end">
-            <div className="table__header--content ">ACTIONS</div>
-          </th>
-        </tr>
-      </thead>
-      {warehouses.map(
-        ({
-          id,
-          city,
-          country,
-          address,
-          contact_name,
-          contact_phone,
-          contact_email,
-          warehouse_name,
-        }, index) => (
-          <tbody key={id} className="table__body">
-            <tr className={`table__body--row ${index === (warehouses.length - 1) && "remove-bottom-border"}`}>
-              <td className="table__data--cells table__data--location">
-                <h4 className="table__data--header table__data--hidden ">
-                  WAREHOUSE
-                </h4>
-                <Link to={`/warehouse/${id}`} className="table__data--link">
-                  {warehouse_name}
-                  <img src={chevronRight} alt="Arrow Point To Right" />
-                </Link>
-              </td>
-              <td className="table__data--cells table__data--name">
-                <h2 className="table__data--header table__data--hidden">
-                  CONTACT NAME
-                </h2>
-                <p className="table__data--content">{contact_name}</p>
-              </td>
-              <td className="table__data--cells">
-                <h2 className="table__data--header table__data--hidden">
-                  ADDRESS
-                </h2>
-                <hgroup className="table__data--address">
-                  <p className="table__data--content">{address}</p>
-                  <p className="table__data--content">
-                    {city}, {country}
-                  </p>
-                </hgroup>
-              </td>
-              <td className="table__data--cells table__data--contact">
-                <h2 className="table__data--header table__data--hidden">
-                  CONTACT INFORMATION
-                </h2>
-                <hgroup>
-                  <p className="table__data--content table__data--margin">{contact_phone}</p>
-                  <Link
-                    to="mailto:paujla@instock.com"
-                    className="table__data--content table__data--email"
-                  >
-                    {contact_email}
+    <>
+      {isModalOpen && <Modal onClose={handleOpenModal} onDelete={handleDeleteWarehouse} />}
+
+      <table className="table">
+        <thead className="table__header">
+          <tr className="table__header--row">
+            <th className="table__header--cells">
+              <div className="table__header--content">
+                WAREHOUSE
+                <img
+                  src={sortIcon}
+                  className="table__header--icon"
+                  alt="Two Arrows Point Up and Down"
+                />
+              </div>
+            </th>
+            <th className="table__header--cells">
+              <div className="table__header--content">
+                ADDRESS
+                <img
+                  src={sortIcon}
+                  className="table__header--icon"
+                  alt="Two Arrows Point Up and Down"
+                />
+              </div>
+            </th>
+            <th className="table__header--cells">
+              <div className="table__header--content">
+                CONTACT NAME
+                <img
+                  src={sortIcon}
+                  className="table__header--icon"
+                  alt="Two Arrows Point Up and Down"
+                />
+              </div>
+            </th>
+            <th className="table__header--cells">
+              <div className="table__header--content">
+                CONTACT INFORMATION
+                <img
+                  src={sortIcon}
+                  className="table__header--icon"
+                  alt="Two Arrows Point Up and Down"
+                />
+              </div>
+            </th>
+            <th className="table__header--end">
+              <div className="table__header--content ">ACTIONS</div>
+            </th>
+          </tr>
+        </thead>
+        {warehouses.map(
+          (
+            {
+              id,
+              city,
+              country,
+              address,
+              contact_name,
+              contact_phone,
+              contact_email,
+              warehouse_name,
+            },
+            index
+          ) => (
+            <tbody key={id} className="table__body">
+              <tr
+                className={`table__body--row ${
+                  index === warehouses.length - 1 && "remove-bottom-border"
+                }`}
+              >
+                <td className="table__data--cells table__data--location">
+                  <h4 className="table__data--header table__data--hidden ">
+                    WAREHOUSE
+                  </h4>
+                  <Link to={`/warehouse/${id}`} className="table__data--link">
+                    {warehouse_name}
+                    <img src={chevronRight} alt="Arrow Point To Right" />
                   </Link>
-                </hgroup>
-              </td>
-              <td className="table__data--actions">
-                <button className="table__data--delete">
-                  <img src={trashIcon} alt="Garbage Red Color Icon" />
-                </button>
-                <button className="table__data--edit">
-                  <img src={editIcon} alt="Garbage Red Color Icon" />
-                </button>
-              </td>
-            </tr>
-          </tbody>
-        )
-      )}
-    </table>
+                </td>
+                <td className="table__data--cells table__data--name">
+                  <h2 className="table__data--header table__data--hidden">
+                    CONTACT NAME
+                  </h2>
+                  <p className="table__data--content">{contact_name}</p>
+                </td>
+                <td className="table__data--cells">
+                  <h2 className="table__data--header table__data--hidden">
+                    ADDRESS
+                  </h2>
+                  <hgroup className="table__data--address">
+                    <p className="table__data--content">{address}</p>
+                    <p className="table__data--content">
+                      {city}, {country}
+                    </p>
+                  </hgroup>
+                </td>
+                <td className="table__data--cells table__data--contact">
+                  <h2 className="table__data--header table__data--hidden">
+                    CONTACT INFORMATION
+                  </h2>
+                  <hgroup>
+                    <p className="table__data--content table__data--margin">
+                      {contact_phone}
+                    </p>
+                    <Link
+                      to="mailto:paujla@instock.com"
+                      className="table__data--content table__data--email"
+                    >
+                      {contact_email}
+                    </Link>
+                  </hgroup>
+                </td>
+                <td className="table__data--actions">
+                  <button
+                    className="table__data--delete"
+                    onClick={handleOpenModal}
+                  >
+                    <img src={trashIcon} alt="Garbage Red Color Icon" />
+                  </button>
+                  <Link className="table__data--edit">
+                    <img src={editIcon} alt="Garbage Red Color Icon" />
+                  </Link>
+                </td>
+              </tr>
+            </tbody>
+          )
+        )}
+      </table>
+    </>
   );
 };
--- src/pages/Warehouses/Warehouses.jsx
+++ src/pages/Warehouses/Warehouses.jsx
@@ -7,16 +7,13 @@ import searchIcon from "../../assets/icons/search-24px.svg";
 
 import { Card } from "../../components/Card/Card";
 import { CardHeader } from "../../components/CardHeader/CardHeader";
-import { CardFooter } from "../../components/CardFooter/CardFooter";
 import { DynamicInput } from "../../components/DynamicInput/DynamicInput";
 import { DynamicButton } from "../../components/DynamicButton/DynamicButton";
 import { WarehouseList } from "../../components/WarehouseList/WarehouseList";
 import { LoadingSpinner } from "../../components/LoadingSpinner/LoadingSpinner";
-import { Modal } from "../../components/Modal/Modal";
 
 export const Warehouses = () => {
   const [warehouses, setWarehouses] = useState([]);
-  const [isModalOpen, setIsModalOpen] = useState(false);
 
   useEffect(() => {
     getAllWarehouse(setWarehouses);
@@ -24,12 +21,8 @@ export const Warehouses = () => {
 
   if (!warehouses) return <LoadingSpinner />;
 
-  const handleOpenModal = () => setIsModalOpen(!isModalOpen);
-
   return (
     <>
-      {isModalOpen && <Modal onClose={handleOpenModal} />}
-
       <main className="main">
         <Card>
           <CardHeader flexStyle="flexCol" browserName="Warehouse">
@@ -42,10 +35,9 @@ export const Warehouses = () => {
             <DynamicButton
               variant="add"
               addButtonName="Add New Warehouse"
-              onClick={handleOpenModal}
             />
           </CardHeader>
-          <WarehouseList warehouses={warehouses} />
+          <WarehouseList  warehouses={warehouses} />
         </Card>
       </main>
     </>
