3e27c86d1f1c43388560062bbcaf66d47fd3ea52
achris-alonzo30 <achris.alonzo30@gmail.com>
2024-06-29 08:07:48 -0300

    add new page NewInventory and place the form for create new inventory on the page

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ import NewWarehouse from "./pages/NewWarehouse/NewWarehouse";
 
 import { Inventory } from "./pages/Inventory/Inventory";
 import { InventoryDetails } from "./pages/InventoryDetails/InventoryDetails";
+import { NewInventory } from "./pages/NewInventory/NewInventory";
 
 function App() {
   return (
@@ -26,7 +27,7 @@ function App() {
         <Route path="/warehouse/:warehouseId/edit" element={<EditWarehouseItem />} />
        
         <Route path="/inventory" element={<Inventory />} />
-        <Route path="/inventory/post" element={<AddInventoryItem />} />
+        <Route path="/inventory/post" element={<NewInventory />} />
         <Route path="/inventory/:inventoryId" element={<InventoryDetails />} />
         <Route path="/inventory/:inventoryId/edit" element={<EditInventoryItem />} />
       </Routes>
---
+++ src/pages/NewInventory/NewInventory.jsx
@@ -0,0 +1,22 @@
+import "./NewInventory.scss";
+
+import { Card } from "../../components/Card/Card";
+import { CardHeader } from "../../components/CardHeader/CardHeader";
+import AddInventoryItem from "../../components/AddInventoryItem/AddInventoryItem";
+
+
+export const NewInventory = () => {
+  return (
+    <main className="main">
+      <Card>
+        <CardHeader
+          flexStyle="flexRow"
+          browserName="Add New Inventory Item"
+          tabletHeaderBorder={true}
+          withArrow
+        />
+        <AddInventoryItem />
+      </Card>
+    </main>
+  );
+};
